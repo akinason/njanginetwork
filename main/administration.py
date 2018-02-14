93c4480dc1e42bfd62bf4c0dde61e44883256b3f
gloxon <gloxongp@gmail.com>
2018-02-14 18:15:33 +0100

    added a module to send mtn_mass_sms, orange_mass_sms, mass_sms

--- main/administration.py
+++ main/administration.py
@@ -1,9 +1,11 @@
 
 from njangi.core import create_user_levels, add_user_to_njangi_tree
+from django.contrib.auth import get_user_model
+from mailer import services as mailer_services
+from django.db.models import Q
 
 
 def create_admin_user(username, password):
-    from django.contrib.auth import get_user_model
     failed_reason = ''
     if get_user_model().objects.filter(username=username).exists():
         failed_reason = 'Username already exist.'
@@ -28,3 +30,56 @@ def create_admin_user(username, password):
         add_user_to_njangi_tree(user=user)
         create_user_levels(user=user)
         return user
+
+
+def send_mtn_mass_sms(body):
+    number_list = []
+    users = get_user_model().objects.filter(tel1__isnull=False, is_admin=False)
+    for user in users:
+        try:
+            if user.tel1:
+                tel = user.tel1.as_international
+                _tel = tel.replace("+", "").replace(" ", "")
+                number_list.append(_tel)
+        except Exception:
+            pass
+    response = mailer_services.send_1s2u_mass_sms(to_numbers=number_list, body=body)
+    return response
+
+
+def send_orange_mass_sms(body):
+    number_list = []
+    users = get_user_model().objects.filter(tel2__isnull=False, is_admin=False)
+    for user in users:
+        try:
+            if user.tel1:
+                tel = user.tel2.as_international
+                _tel = tel.replace("+", "").replace(" ", "")
+                number_list.append(_tel)
+        except Exception:
+            pass
+    response = mailer_services.send_1s2u_mass_sms(to_numbers=number_list, body=body)
+    return response
+
+
+def send_mass_sms(body):
+    number_list = []
+    tel = ''
+    users = get_user_model().objects.filter(Q(tel1__isnull=False) | Q(tel2__isnull=False), is_admin=False)
+    for user in users:
+        try:
+            if user.tel1 and not user.tel2:
+                tel = user.tel1.as_international
+            elif not user.tel1 and user.tel2:
+                tel = user.tel2.as_international
+            elif user.tel1 and user.tel2:
+                tel = user.tel1.as_international
+            else:
+                pass
+            _tel = tel.replace("+", "").replace(" ", "")
+            number_list.append(_tel)
+        except Exception:
+            pass
+    response = mailer_services.send_1s2u_mass_sms(to_numbers=number_list, body=body)
+    return response
+
