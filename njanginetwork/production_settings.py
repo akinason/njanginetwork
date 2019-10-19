5ca353c1951a5ab8a2869c4537c9b509875d89c8
Awa Kinason <kinason42@gmail.com>
2019-10-19 11:35:30 +0100

    added production_settings file

---
+++ njanginetwork/production_settings.py
@@ -0,0 +1,8 @@
+
+
+# Monetbil Authentication info
+
+MONETBIL_NJANGI_NETWORK_SERVICE_KEY = 'IUQTPgsgejfni20P6NntjVfHkMTe4iuY'
+MONETBIL_NJANGI_NETWORK_SERVICE_SECRET = 'IA7CBtfvsrOXU95jZXsrYybeA1QUzkUALco7xnBdcFzYnb7xicnehZJVkqBUTBdl'
+
+PROTOCOL = 'https://'
